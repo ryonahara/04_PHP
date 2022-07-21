afbbb820e875081be24a13d503edf5663f22c2b9
ryonahara <r.yonahara33@gmail.com>
2022-07-21 11:02:26 +0900

    add:05 di

---
+++ 04/echo.php
@@ -0,0 +1,15 @@
+<?php
+//echo = 10;
+echo $a = 10;
+//echo $10;
+//echo 1 0;
+//echo １０;
+//echo 1.0;
+echo 1,0;
+echo '1'.'0';
+//echo 1 + 0;
+echo (10);
+//echo {$a = 10};
+//$a = 10; echo '$a';
+$a = 10; echo     ($a);
+?>
