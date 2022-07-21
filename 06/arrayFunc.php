20eba43d9cac3c6487a9ad0f9b7833c1f3595b17
ryonahara <r.yonahara33@gmail.com>
2022-07-21 15:58:46 +0900

    fix:arrayFunc echoの冗長な丸括弧を削除

--- 06/arrayFunc.php
+++ 06/arrayFunc.php
@@ -2,6 +2,6 @@
 $domain = 'http://example.com?';
 $text = 'PHP　MySQL　Laravel';
 
-echo ($domain . http_build_query(explode('　', $text)));
+echo $domain . http_build_query(explode('　', $text));
 
 /* http://example.com?0=PHP&1=MySQL&2=Laravel */
