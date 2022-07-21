a85ac2bb4a09b71887832b8d8ccf03d9211e3196
ryonahara <r.yonahara33@gmail.com>
2022-07-21 15:52:42 +0900

    fix:arrayFunc 代入使わず１行に修正、出力をechoに修正

--- 06/arrayFunc.php
+++ 06/arrayFunc.php
@@ -1,11 +1,8 @@
 <?php
 $domain = 'http://example.com?';
-$text = 'PHP　MySQL　Laravel';
 
-$textArray = explode('　', $text);
-$textURL = 'http://example.com?' . http_build_query($textArray);
+$textURL = $domain . http_build_query(explode('　', 'PHP　MySQL　Laravel'));
 
-echo '<pre>';
-print_r($textURL);
-echo '</pre>';
+echo ($textURL);
+
 /* http://example.com?0=PHP&1=MySQL&2=Laravel */
