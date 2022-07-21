c7a994a65338e30c3b1a58795a9cf3ef45a880f0
ryonahara <108518110+ryonahara@users.noreply.github.com>
2022-07-21 11:15:52 +0900

    Delete 04 directory

--- 04/const.php
+++
@@ -1,7 +0,0 @@
-<?php
-const DB_HOST = 'localhost';
-const DB_NAME = 'mydb';
-const DB_USER = 'root';
-const DB_PASS = 'secret';
-$pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS );
-?>
