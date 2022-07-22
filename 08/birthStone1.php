4f3112ef0c3928d0daaae21c1cbcf89d4bcab17a
ryonahara <r.yonahara33@gmail.com>
2022-07-23 01:02:57 +0900

    add:08-4 birthstone1,2

---
+++ 08/birthStone1.php
@@ -0,0 +1,52 @@
+<?php
+//誕生月と誕生石一覧
+$birthStones = [
+    '1'  => 'ガーネット',
+    '2'  => 'アメジスト',
+    '3'  => 'アクアマリン',
+    '4'  => 'ダイヤモンド',
+    '5'  => 'エメラルド',
+    '6'  => 'パール',
+    '7'  => 'ルビー',
+    '8'  => 'ペリドット',
+    '9'  => 'サファイア',
+    '10' => 'オパール',
+    '11' => 'トパーズ',
+    '12' => 'ターコイズ'
+    ];
+$month = $_POST['month'];
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>誕生石</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <h1>誕生石</h1>
+        <p><?=$month?>月の誕生石は<?=$birthStones[$month]?>です！</p>
+        <p>誕生月を選んでください:
+            <select name="month" id="1" value="<?=$month?>">
+                <option value="1">1月</option>
+                <option value="2">2月</option>
+                <option value="3">3月</option>
+                <option value="4">4月</option>
+                <option value="5">5月</option>
+                <option value="6">6月</option>
+                <option value="7">7月</option>
+                <option value="8">8月</option>
+                <option value="9">9月</option>
+                <option value="10">10月</option>
+                <option value="11">11月</option>
+                <option value="12">12月</option>
+            </select>
+            <input type="submit" value="送信">
+        </p>
+    </form>
+</body>
+
+</html>
