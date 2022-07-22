516b547d3ddd9c813a6fce16523ae9ad48035bd5
ryonahara <r.yonahara33@gmail.com>
2022-07-22 13:59:35 +0900

    add:08-3:cart3

---
+++ 08/cart3.php
@@ -0,0 +1,73 @@
+<?php
+//商品と値段
+$goods = [
+    ['和風柄レターセット', 980],
+    ['毛筆ペン(細字)', 280]
+];
+//数量
+$count1 = $_POST['count1'];
+$count2 = $_POST['count2'];
+//小計
+$subTotal1 = $goods[0][1] * $count1;
+$subTotal2 = $goods[1][1] * $count2;
+//合計
+$total = $subTotal1 + $subTotal2;
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ショッピングカート</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 600px;
+        }
+
+        th,
+        td {
+            border: 1px solid #666666;
+            padding: 4px;
+        }
+
+        th {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>ショッピングカート</h1>
+    <form action="" method="post">
+        <table>
+            <tr>
+                <th>商品名</th>
+                <th>単価</th>
+                <th>数量</th>
+                <th>小計</th>
+            </tr>
+            <tr>
+                <td><?= $goods[0][0] ?></td>
+                <td><?= $goods[0][1] ?>円</td>
+                <td><input type="text" name="count1" value="<?= htmlspecialchars($count1, ENT_QUOTES, 'UTF-8') ?>"></td>
+                <td><?= $subTotal1 ?>円</td>
+            </tr>
+            <tr>
+                <td><?= $goods[1][0] ?></td>
+                <td><?= $goods[1][1] ?>円</td>
+                <td><input type="text" name="count2" value="<?= htmlspecialchars($count2, ENT_QUOTES, 'UTF-8') ?>"></td>
+                <td><?= $subTotal2 ?>円</td>
+            </tr>
+            <tr>
+                <th colspan="3">合計</th>
+                <td><?= $total ?>円</td>
+            </tr>
+        </table>
+        <p><input type="submit" value="更新"></p>
+    </form>
+</body>
+
+</html>
