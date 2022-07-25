502f1893b9c579eb7522eaf262c663a7646b662f
ryonahara <r.yonahara33@gmail.com>
2022-07-25 14:52:07 +0900

    fix:09-6 冗長な初期化を削除、判定分を入れ子に、htmlの微調整

--- 09/score.php
+++ 09/score.php
@@ -2,24 +2,23 @@
 $score = '';
 if (!empty($_POST)) {
     $score = $_POST['score'];
-}
-$result = '';
-
-if (!is_numeric($score)) {
-    $result =  '数値を入力してください';
-} elseif (0 > $score || $score > 100) {
-    $result =  '不正な点数です';
-} else {
-    if ($score == 100) {
-        $result =  '満点おめでとう！';
-    } elseif ($score >= 80) {
-        $result =  '素晴らしいです！';
-    } elseif ($score >= 60) {
-        $result =  '合格です';
+    if (!is_numeric($score)) {
+        $result =  '数値を入力してください';
+    } elseif (0 > $score || $score > 100) {
+        $result =  '不正な点数です';
     } else {
-        $result =  '不合格です';
+        if ($score == 100) {
+            $result =  '満点おめでとう！';
+        } elseif ($score >= 80) {
+            $result =  '素晴らしいです！';
+        } elseif ($score >= 60) {
+            $result =  '合格です';
+        } else {
+            $result =  '不合格です';
+        }
     }
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -32,12 +31,14 @@ if (!is_numeric($score)) {
 </head>
 
 <body>
+    <h1>テスト結果判定</h1>
     <form action="" method="post">
-        <p><input type="text" name="score" value="<?= htmlspecialchars($score, ENT_QUOTES, 'UTF-8'); ?>"></p>
-        <p><input type="submit" value="送信"></p>
+        <p>点数:
+            <input type="text" name="score" size="3" value="<?= htmlspecialchars($score, ENT_QUOTES, 'UTF-8'); ?>">
+            <input type="submit" value="判定"></p>
     </form>
     <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
-        <p><?= $result ?></p>
+        <p>判定:<?= $result ?></p>
     <?php endif; ?>
 </body>
 
