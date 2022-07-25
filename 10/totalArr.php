9550ea13d0fd68b3732d91e0fdba8ba5b1d425d8
ryonahara <r.yonahara33@gmail.com>
2022-07-25 18:46:13 +0900

    add:10-5 totalArr

---
+++ 10/totalArr.php
@@ -0,0 +1,9 @@
+<?php
+$priceList = [298, 348, 198, 680, 98, 980, 498, 640];
+$total = 0;
+
+for ($i = 0; $i < count($priceList); $i++) {
+    $total += $priceList[$i];
+}
+$total *= 1.1;
+echo $total . '円';
