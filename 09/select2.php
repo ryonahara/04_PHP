467e3e48aec9181eac939182d4bda8523075d150
ryonahara <r.yonahara33@gmail.com>
2022-07-25 15:49:29 +0900

    add:09-8 select2

---
+++ 09/select2.php
@@ -0,0 +1,29 @@
+<?php
+$item = 'バナナ';
+if (!empty($_POST)) {
+    $item = $_POST['item'];
+}
+
+?>
+<html>
+
+<body>
+    <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') : ?>
+        <p><?=$item?>が選択されました。</p>
+    <?php endif; ?>
+    <form action="" method="post">
+        <p>
+            <select name="item">
+                <option <?php if($item == 'リンゴ') echo 'selected'; ?>>リンゴ</option>
+                <option <?php if($item == 'バナナ') echo 'selected'; ?>>バナナ</option>
+                <option <?php if($item == 'ぶどう') echo 'selected'; ?>>ぶどう</option>
+                <option <?php $item == 'メロン' ? print 'selected' : ''; ?>>メロン</option>
+                <option <?php $item == 'みかん' ? print 'selected' : ''; ?>>みかん</option>
+                <option <?php $item == 'レモン' ? print 'selected' : ''; ?>>レモン</option>
+            </select>
+        </p>
+        <p><input type="submit" value="送信"></p>
+    </form>
+</body>
+
+</html>
