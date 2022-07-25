0e2c606e4b90aac66ab5dd19b972eff9c2d6b612
ryonahara <r.yonahara33@gmail.com>
2022-07-25 17:48:44 +0900

    fix:10-3 空白明け、php終了タグ削除、論理演算子使用

--- 10/fizzbuzz.php
+++ 10/fizzbuzz.php
@@ -1,13 +1,12 @@
 <?php
 for ($i = 1; $i <= 100; $i++) {
     echo $i;
-    if ($i % 15 == 0) {
+    if ($i % 3 == 0 && $i % 5 == 0) {
         echo ':FizzBuzz';
-    }elseif ($i % 5 == 0) {
+    } elseif ($i % 5 == 0) {
         echo ':Buzz';
-    }elseif ($i % 3 == 0) {
+    } elseif ($i % 3 == 0) {
         echo ':Fizz';
     }
     echo '<br>';
 }
-?>
