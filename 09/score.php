5e5f238e0caf4ccce08273fd3d284bc5bd1e6c99
ryonahara <r.yonahara33@gmail.com>
2022-07-25 12:06:53 +0900

    add:09-4 score

--- 09/score.php
+++ 09/score.php
@@ -1,11 +1,17 @@
 <?php
-$score = 50;
-if ($score == 100) {
-    echo '満点おめでとう！';
-} elseif ($score >= 80) {
-    echo '素晴らしいです！';
-} elseif ($score >= 60) {
-    echo '合格です';
+$score = '';
+if (!is_numeric($score)) {
+    echo '数値を入力してください';
+}elseif (0 > $score ||$score > 100) {
+    echo '不正な点数です';
 } else {
-    echo '不合格です';
+    if ($score == 100) {
+        echo '満点おめでとう！';
+    } elseif ($score >= 80) {
+        echo '素晴らしいです！';
+    } elseif ($score >= 60) {
+        echo '合格です';
+    } else {
+        echo '不合格です';
+    }
 }
