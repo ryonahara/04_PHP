7b70befabfe388f4b9e0aafd0708a78c9c0b0376
ryonahara <r.yonahara33@gmail.com>
2022-07-26 18:11:07 +0900

    fix:11-4 税込価格をリターン

--- 11/tax.php
+++ 11/tax.php
@@ -1,6 +1,6 @@
 <?php
 $prices = [298, 129, 198, 274, 625, 273, 296, 325, 200, 127, 398];
-// 3,457円
+// 3,457円(3143 + 314(10%))
 
 echo number_format(getPriceInTax($prices, 10)) . '円';
 
@@ -13,7 +13,5 @@ function getPriceInTax($prices, $tax = 8)
     //税込価格
     $priceInTax = $total * (1 + $tax / 100);
 
-    //消費税
-    $taxPrice = $priceInTax - $total;
-    return floor($taxPrice);
+    return floor($priceInTax);
 }
