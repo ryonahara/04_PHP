fe8416907d24b4a137a2508fa0859e8a99ecbaef
ryonahara <r.yonahara33@gmail.com>
2022-07-28 14:50:02 +0900

    fix:13-1 関数内で曜日を取得

--- 13/modify.php
+++ 13/modify.php
@@ -1,24 +1,19 @@
 <?php
 
-$weekday = ['日', '月', '火', '水', '木', '金', '土'];
-
-
 //2022年2月末日
 $d1 = new DateTime('last day of February 2022');
-$w1 = $weekday[$d1->format('w')];
 
 //現在時刻の10日前
 $d2 = new DateTime('today');
 $d2->modify('-10 days');
-$w2 = $weekday[$d2->format('w')];
 
 //差を算出
 $interval = $d1->diff($d2);
 $days = $interval->days;
 
-//和製の日付に変換
-$jpd1 = getJpDate($d1->format('Y年m月d日'), $w1);
-$jpd2 = getJpDate($d2->format('Y年m月d日'), $w2);
+//曜日の取得と和製の日付に変換
+$jpd1 = getJpDate($d1);
+$jpd2 = getJpDate($d2);
 
 //結果を出力
 $invert = $interval->invert;
@@ -33,14 +28,19 @@ if ($days == 0) {
 }
 
 /**
- * 和製の日付の文字列を返す
+ * 曜日を取得し、和製の日付の文字列を返す
  *
- * @param string|null $d
- * @param string|null $w
+ * @param object|null $d
  * @return string
  */
-function getJpDate(?string $d, ?string $w): string
+function getJpDate(?object $d): string
 {
-    $jpDate = $d . '(' . $w . ')';
+    //曜日の取得
+    $weekday = ['日', '月', '火', '水', '木', '金', '土'];
+    $w = $weekday[$d->format('w')];
+
+    //文字列結合
+    $jpDate = $d->format('Y年m月d日') . '(' . $w . ')';
+
     return $jpDate;
 }
