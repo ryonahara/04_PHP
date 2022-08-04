ef437e5ad5f0a3fb08b1e7f9fe4e8c512013d7f4
ryonahara <r.yonahara33@gmail.com>
2022-08-04 23:45:56 +0900

    add:07-5 addMember.php 追加仕様

---
+++ 20/Validation.php
@@ -0,0 +1,48 @@
+<?php
+
+class Validation
+{
+
+    /**
+     * 入力された氏名の判定結果を元にエラーメッセージを返す
+     *
+     * @param string|null $name
+     * @return string|null
+     */
+    public function validName(?string $name): ?string
+    {
+        if (empty($name)) {
+            return '※氏名を入力してください';
+        } elseif (mb_strlen($name, 'utf8') > 10) {
+            return '※氏名10文字以内で入力してください';
+        } else {
+            return NULL;
+        }
+    }
+
+
+    /**
+     * 入力された年齢の判定結果を元に空値かエラーメッセージの配列を返す
+     *
+     * @param string|null $age
+     * @return array
+     */
+    public function validAge(?string $age): ?array
+    {
+        /* 初期化 */
+        $res['err'] = '';
+
+        if ($age === '' || mb_ereg_match('/^(\s|　)+$/', $age)) {
+            $res['age'] = null;
+        } elseif (!is_numeric($age) || $age < 0) {
+            /* 判定に引っかかっても値をそのまま返す */
+            $res['age'] = $age;
+            $res['err'] = '※年齢は0以上の数値を入力してください';
+        } else {
+            /* 成功時 */
+            $res['age'] = $age;
+            $res['err'] = '';
+        }
+        return $res;
+    }
+}
--- 20/addMember.flag.php
+++ 20/addMember.flag.php
@@ -3,33 +3,38 @@
 declare(strict_types=1);
 require_once dirname(__FILE__) . '/db.inc.php';
 require_once dirname(__FILE__) . '/util.inc.php';
-require_once dirname(__FILE__) . '/Validation.php';
 
 // 値の初期化
 $name    = '';
 $age     = '';
 $address = '';
-$error['name'] = null;
-$error['age'] = null;
+$isValidated = false;
 
 if (!empty($_POST)) {
     // 送信値の取得
     $name    = $_POST['name'];
     $age     = $_POST['age'];
     $address = $_POST['address'];
+    $isValidated = true;
 
     //入力値判定
-    $v = new Validation();
-    $error['name'] = $v->validName($name);
+    if (empty($name)) {
+        $nameError = '※氏名を入力してください';
+        $isValidated = false;
+    } elseif (mb_strlen($name, 'utf8') > 10) {
+        $nameError = '※氏名10文字以内で入力してください';
+        $isValidated = false;
+    }
+    if ($age === '' || mb_ereg_match('/^(\s|　)+$/', $age)) {
+        $age = null;
+        $isValidated = false;
+    } elseif (!is_numeric($age) || $age < 0) {
+        $ageError = '※年齢は0以上の数値を入力してください';
+        $isValidated = false;
+    }
 
-    /* メソッドの戻り値となる配列を展開 */
-    $res          = $v->validAge($age);
-    $age          = $res['age'];
-    $error['age'] = $res['err'];
 
-
-    if (($error['name'] == null) && ($error['age'] == null) && ($age != null)) {
-
+    if ($isValidated == true) {
         try {
             // データ登録
             $pdo = dbConnect();
@@ -42,11 +47,11 @@ if (!empty($_POST)) {
             $stmt->bindValue(':age', (int)$age, PDO::PARAM_INT);
             $stmt->bindValue(':address', $address, PDO::PARAM_STR);
             $stmt->execute();
+
         } catch (PDOException $e) {
             header('Content-Type: text/plain; charset=UTF-8', true, 500);
             exit($e->getMessage());
         }
-
     }
 }
 
@@ -71,21 +76,19 @@ if (!empty($_POST)) {
 <body>
     <h1>会員登録</h1>
     <p><a href="member.php">会員一覧に戻る</a></p>
-
-    <?php if (($error['name'] == null) && ($error['age'] == null) && ($age != null)) : ?>
+    <?php if ($isValidated == true) : ?>
         <p>登録完了しました。</p>
     <?php else : ?>
         <form action="" method="post" novalidate>
             <p>氏名：<input type="text" name="name" value="<?= h($name) ?>">
-                <?php if (!empty($error['name'])) : ?>
-                    <span class="error"><?= $error['name'] ?></span>
+                <?php if (isset($nameError)) : ?>
+                    <span class="error"><?= $nameError ?></span>
                 <?php endif; ?>
             </p>
-            <p>年齢：<input type="text" name="age" value="<?= h($age) ?>">
-                <?php if (!empty($error['age'])) : ?>
-                    <span class="error"><?= $error['age'] ?></span>
-                <?php endif; ?>
-            </p>
+            <p>年齢：<input type="text" name="age" value="<?= h($age) ?>"></p>
+            <?php if (isset($ageError)) : ?>
+                <span class="error"><?= $ageError ?></span>
+            <?php endif; ?>
             <p>住所：<input type="text" name="address" value="<?= h($address) ?>"></p>
             <p><input type="submit" value="送信"></p>
         </form>
